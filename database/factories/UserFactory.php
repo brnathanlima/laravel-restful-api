0b4da26d32316efbc449a6078fd451a4bbb276ff
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-02-15 08:28:31 -0300

    Adjusting user factory to only generate secret for verified users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'verified' => $verified = $this->faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
-            'verification_token' => $verified == User::UNVERIFIED_USER ?: User::generateVerificationCode(),
+            'verification_token' => $verified == User::UNVERIFIED_USER ? null : User::generateVerificationCode(),
             'admin' => $this->faker->randomElement([User::ADMIN_USER, User::REGULAR_USER])
         ];
     }
