e5ba02fb407900f27a4653540888ca7193551f6a
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-02-25 08:00:43 -0300

    Adding buyer global scope

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -14,7 +14,7 @@ class BuyerController extends ApiController
      */
     public function index()
     {
-        $buyers = Buyer::has('transactions')->get();
+        $buyers = Buyer::all();
 
         return $this->showAll($buyers);
     }
@@ -25,10 +25,8 @@ class BuyerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Buyer $buyer)
     {
-        $buyer = Buyer::has('transactions')->findOrFail($id);
-
         return $this->showOne($buyer);
     }
 }
