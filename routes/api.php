a5d9cc59ee7def30b12c1a41f3672fc13f890ae6
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-02-26 07:53:06 -0300

    Adjusting categories routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('buyers', BuyerController::class, ['only' => ['index', 'show']]);
 Route::resource('sellers', SellerController::class, ['only' => ['index', 'show']]);
-Route::resource('categorys', CategoryController::class, ['except' => ['create', 'edit']]);
+Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
 Route::resource('products', ProductController::class, ['only' => ['index', 'show']]);
 Route::resource('transactions', TransactionController::class, ['only' => ['index', 'show']]);
 Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
