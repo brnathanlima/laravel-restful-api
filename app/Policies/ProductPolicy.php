f940a25e8ee767eb4a0e9bc44e73f288771e18df
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-03-28 20:38:22 -0300

    Implementing the restrictions over product

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Product  $product
+     * @return mixed
+     */
+    public function addCategory(User $user, Product $product)
+    {
+        return $user->id === $product->seller->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Product  $product
+     * @return mixed
+     */
+    public function removeCategory(User $user, Product $product)
+    {
+        return $user->id === $product->seller->id;
+    }
+}
