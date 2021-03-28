17eddc0941f05858c40381e02f2d57eb8e5d8183
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-03-28 13:26:06 -0300

    Restricting the actions that need the manage-account scope

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -18,6 +18,7 @@ class UserController extends ApiController
         $this->middleware('client.credentials')->only(['store', 'resend']);
         $this->middleware('auth:api')->except(['store', 'resend', 'verify']);
         $this->middleware('transform.input:' . UserTransformer::class)->only(['store', 'update']);
+        $this->middleware('scope:manage-account')->only(['show', 'update']);
     }
 
     /**
