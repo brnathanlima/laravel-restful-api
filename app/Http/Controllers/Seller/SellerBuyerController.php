11d2ac9d09fbc41bfb1cf35459b98a2593d660d2
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-04-09 20:44:43 -0300

    Documenting seller's buyers listing

--- app/Http/Controllers/Seller/SellerBuyerController.php
+++ app/Http/Controllers/Seller/SellerBuyerController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\ApiController;
-use App\Http\Controllers\Controller;
 use App\Models\Seller;
 use Illuminate\Http\Request;
 
@@ -14,6 +13,82 @@ class SellerBuyerController extends ApiController
         parent::__construct();
     }
 
+    /**
+     * @OA\Get(
+     *      path="/sellers/{seller}/buyers",
+     *      operationId="getSellerTransactionsList",
+     *      tags={"Seller's buyers"},
+     *      summary="Get list of seller's buyers",
+     *      description="Returns list of seller's buyers",
+     *      security={
+     *          {"passport": {}},
+     *      },
+     *      @OA\Parameter(
+     *          name="seller",
+     *          description="Seller id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="order_by",
+     *          description="Transaction property to sort sort the data by",
+     *          required=false,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="per_page",
+     *          description="How many records to return per page",
+     *          required=false,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="page",
+     *          description="Page number",
+     *          required=false,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Returns when seller is not authenticated",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Returns when seller is not authorized to perform this request",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="This action is unauthorized"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      )
+     *  )
+     */
     public function __invoke(Seller $seller)
     {
         $this->allowedAdminAction();
