1254a79799de7f92abaeaaf72dd8a09397a7fdf3
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-05-03 08:54:17 -0300

    Initial setup for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,34 @@
 
 namespace Tests;
 
+use Exception;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations, RefreshDatabase;
+
+    private Generator $faker;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->faker = Factory::create();
+        Artisan::call('passport:install');
+    }
+
+    public function __get($key)
+    {
+        if ($key === 'faker') {
+            return $this->faker;
+        }
+
+        throw new Exception('Unknown key requested');
+    }
 }
