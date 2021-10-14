f52586affac30779ccefc859d04ab09253fca343
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-10-13 22:15:30 -0300

    Retornando nome dos usuários em maiusculo e atualizando testes do UserController

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
 
     public function getNameAttribute($name)
     {
-        return ucwords($name);
+        return strtoupper($name);
     }
 
     public function setEmailAttribute($email)
