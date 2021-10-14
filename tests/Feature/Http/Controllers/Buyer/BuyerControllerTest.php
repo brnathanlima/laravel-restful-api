0108d09a573f41207f6b8dad45cef0b27bd50775
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-10-13 22:13:11 -0300

    Making test for admin is able to list buyers

---
+++ tests/Feature/Http/Controllers/Buyer/BuyerControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Buyer;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class BuyerControllerTest extends TestCase
+{
+    protected $scopes = [];
+
+    public function testAdminIsAbleToListBuyers()
+    {
+        $this->seed();
+
+        $admin = User::factory()->create([
+            'verified' => 0,
+            'verification_token' => User::generateVerificationCode(),
+            'admin' => true
+        ]);
+
+        Passport::actingAs($admin, ['read-general']);
+
+        $this->json('GET', '/buyers')
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'identifier',
+                        'name',
+                        'email',
+                        'isVerified',
+                        'registeredAt',
+                        'lastChange',
+                        'deletedDate',
+                        'links' => [
+                            '*' => [
+                                'rel',
+                                'href'
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+    }
+}
