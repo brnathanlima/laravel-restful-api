01de45b3b62266567a8e9be282b743cd1ccd1ca7
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-10-15 09:48:39 -0300

    Making test for buyer is able to show own profile

--- tests/Feature/Http/Controllers/Buyer/BuyerControllerTest.php
+++ tests/Feature/Http/Controllers/Buyer/BuyerControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Buyer;
 
+use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Http\Response;
 use Laravel\Passport\Passport;
@@ -84,6 +85,63 @@ class BuyerControllerTest extends TestCase
             ->assertExactJson([
                 'error' => 'This action is unauthorized',
                 'code' => Response::HTTP_FORBIDDEN
+            ]);
+    }
+
+    public function testBuyerIsAbleToSeeOwnProfile()
+    {
+        $buyer = User::factory()->create([
+            'verified' => 0,
+            'verification_token' => User::generateVerificationCode(),
+            'admin' => false
+        ]);
+
+        Transaction::factory()->create([
+            'quantity' => 4,
+            'product_id' => 1,
+            'buyer_id' => $buyer->id
+        ]);
+
+        Passport::actingAs($buyer);
+
+        $this->json('GET', "/buyers/{$buyer->id}")
+            ->assertStatus(Response::HTTP_OK)
+            ->assertExactJson([
+                'data' => [
+                    'identifier' => $buyer->id,
+                    'name' => $buyer->name,
+                    'email' => $buyer->email,
+                    'isVerified' => $buyer->verified,
+                    'registeredAt' => $buyer->created_at,
+                    'lastChange' => $buyer->updated_at,
+                    'deletedDate' => $buyer->deleted_at,
+                    'links' => [
+                        [
+                            'rel' => 'self',
+                            'href' => route('buyers.show', $buyer->id)
+                        ],
+                        [
+                            'rel' => 'parent',
+                            'href' => route('users.show', $buyer->id)
+                        ],
+                        [
+                            'rel' => 'buyers.categories',
+                            'href' => route('buyers.categories', $buyer->id)
+                        ],
+                        [
+                            'rel' => 'buyers.products',
+                            'href' => route('buyers.products', $buyer->id)
+                        ],
+                        [
+                            'rel' => 'buyers.sellers',
+                            'href' => route('buyers.sellers', $buyer->id)
+                        ],
+                        [
+                            'rel' => 'buyers.transactions',
+                            'href' => route('buyers.transactions', $buyer->id)
+                        ],
+                    ]
+                ]
             ]);
     }
 }
