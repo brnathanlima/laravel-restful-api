6f932c943d6623a0eff90f05a27cbac25e7c610e
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-10-16 16:27:30 -0300

    Making test to assert that regular user is not able to list buyer sellers

--- tests/Feature/Http/Controllers/Buyer/BuyerSellerControllerTest.php
+++ tests/Feature/Http/Controllers/Buyer/BuyerSellerControllerTest.php
@@ -4,11 +4,8 @@ namespace Tests\Feature\Http\Controllers\Buyer;
 
 use App\Models\Transaction;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
 use Laravel\Passport\Passport;
-use Mockery\Generator\StringManipulation\Pass\Pass;
 use Tests\TestCase;
 
 class BuyerSellerControllerTest extends TestCase
@@ -62,6 +59,36 @@ class BuyerSellerControllerTest extends TestCase
                         ]
                     ]
                 ]
+            ]);
+    }
+
+    public function testRegularUserIsNotAbleToListBuyerSellers()
+    {
+        $regularUser = User::factory()->create([
+            'verified' => 0,
+            'verification_token' => User::generateVerificationCode(),
+            'admin' => false
+        ]);
+
+        $buyer = User::factory()->create([
+            'verified' => 0,
+            'verification_token' => User::generateVerificationCode(),
+            'admin' => false
+        ]);
+
+        Transaction::factory()->create([
+            'quantity' => 1,
+            'product_id' => 1,
+            'buyer_id' => $buyer->id
+        ]);
+
+        Passport::actingAs($regularUser, ['read-general']);
+
+        $this->json('GET', "/buyers/{$buyer->id}/sellers")
+            ->assertForbidden()
+            ->assertExactJson([
+                'error' => 'This action is unauthorized',
+                'code' => Response::HTTP_FORBIDDEN
             ]);
     }
 }
