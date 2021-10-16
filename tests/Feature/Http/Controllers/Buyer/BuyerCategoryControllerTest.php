7040580a12acbb84a07422da2ac0b3eb0c16b704
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-10-16 12:13:07 -0300

    Making test for buyer is not able to see own categories without read general scope

--- tests/Feature/Http/Controllers/Buyer/BuyerCategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Buyer/BuyerCategoryControllerTest.php
@@ -61,4 +61,28 @@ class BuyerCategoryControllerTest extends TestCase
                 ]
             ]);
     }
+
+    public function testBuyerIsNotAbleToSeeOwnCategoriesWithoutReadGeneralScope()
+    {
+        $buyer = User::factory()->create([
+            'verified' => 0,
+            'verification_token' => User::generateVerificationCode(),
+            'admin' => false
+        ]);
+
+        Transaction::factory()->create([
+            'quantity' => 4,
+            'product_id' => 1,
+            'buyer_id' => $buyer->id
+        ]);
+
+        Passport::actingAs($buyer);
+
+        $this->json('GET', "/buyers/{$buyer->id}/categories")
+            ->assertForbidden()
+            ->assertExactJson([
+                'error' => 'Invalid scope(s) provided.',
+                'code' => Response::HTTP_FORBIDDEN
+            ]);
+    }
 }
