2e3c8b8a4a570f1c18dc903332eaeeffa8f65124
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-10-16 16:04:26 -0300

    Making test to assert that buyer is not able to see another buyer products

--- tests/Feature/Http/Controllers/Buyer/BuyerProductControllerTest.php
+++ tests/Feature/Http/Controllers/Buyer/BuyerProductControllerTest.php
@@ -79,4 +79,30 @@ class BuyerProductControllerTest extends TestCase
                 'code' => Response::HTTP_FORBIDDEN
             ]);
     }
+
+    public function testBuyerIsNotAbleToSeeAnotherBuyerProducts()
+    {
+        $buyers = User::factory()->count(2)->create([
+            'verified' => 0,
+            'verification_token' => User::generateVerificationCode(),
+            'admin' => false
+        ]);
+
+        foreach ($buyers as $buyer) {
+            Transaction::factory()->create([
+                'quantity' => 4,
+                'product_id' => 1,
+                'buyer_id' => $buyer->id
+            ]);
+        }
+
+        Passport::actingAs($buyers[0], ['read-general']);
+
+        $this->json('GET', "/buyers/{$buyers[1]->id}/products")
+            ->assertForbidden()
+            ->assertExactJson([
+                'error' => 'This action is unauthorized.',
+                'code' => Response::HTTP_FORBIDDEN
+            ]);
+    }
 }
